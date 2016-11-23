abc6e4bc01e52af3297853186d2c778aa88ca7bf
Greg Di Stefano <greg.distefano+github@gmail.com>
2016-11-23 14:18:57 +0100

    updated main.go to comply with go fmt

--- main.go
+++ main.go
@@ -130,8 +130,8 @@ func clientAuthenticate(c *client.ApiClient, sharedPrivateKey *rsa.PrivateKey) c
 		if authTokenResp, err := authReq.Request(c, backendHost, mgr); err == nil && len(authTokenResp) > 0 {
 			return client.AuthToken(authTokenResp)
 		} else if err != nil {
-            log.Debug("not able to authorize client: ", err)
-        }
+			log.Debug("not able to authorize client: ", err)
+		}
 
 		time.Sleep(5 * time.Second)
 	}
