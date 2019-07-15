db453493167f07609ddc87cbc1e33d2bc13abfc2
Ole Petter <ole.orhagen@northern.tech>
2019-07-15 09:09:53 +0200

    QA-77: Updated vendored client to the 2.0.1 tag
    Changelog: Title
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- vendor/github.com/bmatsuo/lmdb-go/lmdb/lmdb.go
+++ vendor/github.com/bmatsuo/lmdb-go/lmdb/lmdb.go
@@ -4,7 +4,10 @@ fairly low level and are designed to provide a minimal interface that prevents
 misuse to a reasonable extent.  When in doubt refer to the C documentation as a
 reference.
 
-	http://symas.com/mdb/doc/group__mdb.html
+	http://www.lmdb.tech/doc/
+	http://www.lmdb.tech/doc/starting.html
+	http://www.lmdb.tech/doc/modules.html
+
 
 Environment
 
@@ -16,6 +19,12 @@ LMDB recommends setting an environment's size as large as possible at the time
 of creation.  On filesystems that support sparse files this should not
 adversely affect disk usage.  Resizing an environment is possible but must be
 handled with care when concurrent access is involved.
+
+Note that the package lmdb forces all Env objects to be opened with the NoTLS
+(MDB_NOTLS) flag.  Without this flag LMDB would not be practically usable in Go
+(in the author's opinion).  However, even for environments opened with this
+flag there are caveats regarding how transactions are used (see Caveats below).
+
 
 Databases
 
@@ -34,6 +43,7 @@ closed but it is not required.  Typically, applications acquire handles for all
 their databases immediately after opening an environment and retain them for
 the lifetime of the process.
 
+
 Transactions
 
 View (readonly) transactions in LMDB operate on a snapshot of the database at
@@ -50,11 +60,78 @@ transactions do not require explicit calling of Abort/Commit and are provided
 through the Env methods Update, View, and RunTxn.  The BeginTxn method on Env
 creates an unmanaged transaction but its use is not advised in most
 applications.
+
+To provide ACID guarantees, a readonly transaction must acquire a "lock" in the
+LMDB environment to ensure that data it reads is consistent over the course of
+the transaction's lifetime, and that updates happening concurrently will not be
+seen.  If a reader does not release its lock then stale data, which has been
+overwritten by later transactions, cannot be reclaimed by LMDB -- resulting in
+a rapid increase in file size.
+
+Long-running read transactions may cause increase an applications storage
+requirements, depending on the application write workload.  But, typically the
+complete failure of an application to terminate a read transactions will result
+in continual increase file size to the point where the storage volume becomes
+full or a quota has been reached.
+
+There are steps an application may take to greatly reduce the possibility of
+unterminated read transactions.  The first safety measure is to avoid the use
+of Env.BeginTxn, which creates unmanaged transactions, and always use Env.View
+or Env.Update to create managed transactions that are (mostly) guaranteed to
+terminate.  If Env.BeginTxn must be used try to defer a call to the Txn's Abort
+method (this is useful even for update transactions).
+
+	txn, err := env.BeginTxn(nil, 0)
+	if err != nil {
+		// ...
+	}
+	defer txn.Abort() // Safe even if txn.Commit() is called later.
+
+Because application crashes and signals from the operation system may cause
+unexpected termination of a readonly transaction before Txn.Abort may be called
+it is also important that applications clear any readers held for dead OS
+processes when they start.
+
+	numStale, err := env.ReaderCheck()
+	if err != nil {
+		// ...
+	}
+	if numStale > 0 {
+		log.Printf("Released locks for %d dead readers", numStale)
+	}
+
+If an application gets accessed by multiple programs concurrently it is also a
+good idea to periodically call Env.ReaderCheck during application execution.
+However, note that Env.ReaderCheck cannot find readers opened by the
+application itself which have since leaked.  Because of this, the lmdb package
+uses a finalizer to abort unreachable Txn objects.  But of course, applications
+must still be careful not to leak unterminated Txn objects in a way such that
+they fail get garbage collected.
+
+
+Caveats
+
+Write transactions (those created without the Readonly flag) must be created in
+a goroutine that has been locked to its thread by calling the function
+runtime.LockOSThread.  Futhermore, all methods on such transactions must be
+called from the goroutine which created them.  This is a fundamental limitation
+of LMDB even when using the NoTLS flag (which the package always uses).  The
+Env.Update method assists the programmer by calling runtime.LockOSThread
+automatically but it cannot sufficiently abstract write transactions to make
+them completely safe in Go.
+
+A goroutine must never create a write transaction if the application programmer
+cannot determine whether the goroutine is locked to an OS thread.  This is a
+consequence of goroutine restrictions on write transactions and limitations in
+the runtime's thread locking implementation.  In such situations updates
+desired by the goroutine in question must be proxied by a goroutine with a
+known state (i.e.  "locked" or "unlocked").  See the included examples for more
+details about dealing with such situations.
 */
 package lmdb
 
 /*
-#cgo CFLAGS: -pthread -W -Wall -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -O2 -g
+#cgo CFLAGS: -pthread -W -Wall -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -Wno-missing-field-initializers -O2 -g
 #cgo linux,pwritev CFLAGS: -DMDB_USE_PWRITEV
 
 #include "lmdb.h"
